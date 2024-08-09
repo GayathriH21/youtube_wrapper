60bb695d19ee2900d065181d06d0db4a5aa2cb79
GayathriH21 <140702998+GayathriH21@users.noreply.github.com>
2024-08-09 11:04:57 +0530

    Update youtube_transcript.py

--- youtube_wrapper/youtube_transcript.py
+++ youtube_wrapper/youtube_transcript.py
@@ -10,3 +10,13 @@ class YouTubeTranscriptAPI:
             return transcript
         except Exception as e:
             return str(e)
+    def fetch_transcript(self, video_id):
+        try:
+            transcript = YouTubeTranscriptApi.get_transcript(video_id)
+            return transcript
+        except TranscriptsDisabled:
+            return "Transcript is disabled for this video."
+
+    def fetch_transcript_in_language(self, video_id, language_code):
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=[language_code])
+        return transcript        
